229f485fcf41e7cd309b8228ecd7024b5efd2f8b
vvraviteja1 <vvraviteja2@gmail.com>
2025-02-04 20:02:15 +0530

    Add files via upload

---
+++ Daily_tasks/1. V.V.Ravi Teja - Jan 29,30,31 tasks- Arrays, Strings, Class & Object, Static keyword/EMpstatic.java
@@ -0,0 +1,13 @@
+package oop1;
+import org.anurag.staticex;
+@SuppressWarnings("unused")
+public class EMpstatic {
+@SuppressWarnings("static-access")
+public static void main(String args[])
+{
+	staticex.setEmp(58);
+	staticex obj=new staticex();
+	obj.setname("ravi");
+	obj.getName();
+}
+}
