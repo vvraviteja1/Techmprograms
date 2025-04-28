0e844efb5b0a864c62a8740e7e5496c00d295391
vvraviteja1 <vvraviteja2@gmail.com>
2025-04-28 21:58:36 +0530

    Delete Test Project-2_E-Commerce_Platform directory

--- Test Project-2_E-Commerce_Platform/src/main/java/com/ecommerce/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ecommerce;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- Test Project-2_E-Commerce_Platform/src/main/java/com/ecommerce/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce;
-import com.ecommerce.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@SuppressWarnings("unused")
-public interface UserRepository extends JpaRepository<User, String> {
-}
